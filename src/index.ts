7e33fe5f5896b67b53069f39eda3ac32a30fe42c
ARGI-BERRI <me@endroit.net>
2023-10-11 12:41:51 +0900

    change: make constants.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
+import { fate } from './constants';
 import { Env } from './env';
 import { OracleMachine } from './oracle';
-
-const fate = {
-	大吉: 1,
-	中吉: 2,
-	吉: 3,
-	小吉: 4,
-	末吉: 3,
-	凶: 2,
-	大凶: 1,
-};
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
