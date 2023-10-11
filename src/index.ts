983e428579087752bcb137c15a52fe87648446a4
ARGI-BERRI <me@endroit.net>
2023-10-11 12:46:04 +0900

    feat: (wip) now api accepts funny opt

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,16 @@ import { OracleMachine } from './oracle';
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const searchParams = new URL(request.url).searchParams;
+		const options = {
+			funny: searchParams.get('funny') == 'true' ? true : false,
+		};
+
 		const oracle = new OracleMachine(fate);
 		const payload = {
 			id: crypto.randomUUID(),
 			fate: oracle.receive(),
+			options,
 		};
 
 		const json = JSON.stringify(payload, null, 2);
