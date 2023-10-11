808fc202a9f9c416a9cf6ce1d87ba26f8791e656
ARGI-BERRI <me@endroit.net>
2023-10-11 12:50:24 +0900

    chore: prettier

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-import { fate } from './constants';
-import { Env } from './env';
-import { OracleMachine } from './oracle';
+import { fate } from "./constants";
+import { Env } from "./env";
+import { OracleMachine } from "./oracle";
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const searchParams = new URL(request.url).searchParams;
-		const options = {
-			funny: searchParams.get('funny') == 'true' ? true : false,
-		};
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const searchParams = new URL(request.url).searchParams;
+    // NOTE: Zod でバリデーションしたほうがいいけど、面倒なので端折ります
+    const options = {
+      funny: searchParams.get("funny") == "true" ? true : false,
+    };
 
-		const oracle = new OracleMachine(fate);
-		const payload = {
-			id: crypto.randomUUID(),
-			fate: oracle.receive(),
-			options,
-		};
+    const oracle = new OracleMachine(fate);
+    const payload = {
+      id: crypto.randomUUID(),
+      fate: oracle.receive(),
+      options,
+    };
 
-		const json = JSON.stringify(payload, null, 2);
-		return new Response(json, {
-			headers: {
-				'content-type': 'application/json;charset=UTF-8',
-			},
-		});
-	},
+    const json = JSON.stringify(payload, null, 2);
+    return new Response(json, {
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    });
+  },
 };
--- src/oracle.ts
+++ src/oracle.ts
@@ -2,27 +2,27 @@
  * 神託機械を表現するクラス
  */
 export class OracleMachine {
-	/**
-	 * 出目の集合
-	 */
-	fates: string[] = [];
+  /**
+   * 出目の集合
+   */
+  fates: string[] = [];
 
-	/**
-	 * コンストラクタ
-	 * @param fate インデックスが出目、その値が出目の重みである配列
-	 */
-	constructor(fate: { [key: string]: number }) {
-		Object.keys(fate).map((f) => {
-			const weight = fate[f];
-			[...Array(weight)].map(() => this.fates.push(f));
-		});
-	}
+  /**
+   * コンストラクタ
+   * @param fate インデックスが出目、その値が出目の重みである配列
+   */
+  constructor(fate: { [key: string]: number }) {
+    Object.keys(fate).map((f) => {
+      const weight = fate[f];
+      [...Array(weight)].map(() => this.fates.push(f));
+    });
+  }
 
-	/**
-	 * 神託機械から神託を受信する
-	 * @returns コンストラクタに与えた出目のうちいずれか一つ
-	 */
-	receive(): (typeof this.fates)[number] {
-		return this.fates[Math.floor(Math.random() * this.fates.length)];
-	}
+  /**
+   * 神託機械から神託を受信する
+   * @returns コンストラクタに与えた出目のうちいずれか一つ
+   */
+  receive(): (typeof this.fates)[number] {
+    return this.fates[Math.floor(Math.random() * this.fates.length)];
+  }
 }
